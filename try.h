453e2125d15a1896ec3d00b3c85162c598e3547a
codingislearning <amangupta21041995@gmail.com>
2017-07-27 01:24:15 +0530

    Add files via upload

---
+++ try.h
@@ -0,0 +1,23 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#ifndef __TRY_H
+#define __TRY_H
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+void InitAlarm(void);
+void InitSoftwareSerial(void);
+void getNextSample(int* sensorData);
+void SetAlarm();
+void UnSetAlarm();
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif//__DHT22_H
+
